17c37eb3b4169ee7b05306b5279bb5bd892df151
nelson <nelsonvonsd@hotmail.com>
2019-06-20 10:24:35 +0200

    debugs try to initialize simply

--- infrastruct/Helio_struct.py
+++ infrastruct/Helio_struct.py
@@ -9,44 +9,49 @@ HELIO_ID = int
 MAX_BRANCH = 16
 MAX_LENGTH = 128
 
-filename = ''
+filename = '/home/nelson/Dropbox/RWTH/Heuristic optimisation/Project/positions-PS10.csv'
 
 class POS:
+    x:float
+    y:float
     def __init__(self,x,y):
         self.x = x
         self.y = y
         return
-    x:float
-    y:float
+
 
 class CABLE:
     h1: HELIO_ID # ID of heliostat
     h2: HELIO_ID
+    def __init__(self, h1: HELIO_ID, h2: HELIO_ID):
+        self.h1 = h1
+        self.h2 = h2
+        return
 
 class heliostat:
+    pos: POS
+    parent: HELIO_ID
+    child: List[HELIO_ID]
+    size_subtree: int
     def __init__(self, pos:POS):
-        self.pos.x = pos.x
-        self.pos.y = pos.y
+        self.pos = POS(pos.x,pos.y)
         self.parent = -1
         self.child = []
         self.size_subtree = 1
         return
-    pos:POS
-    parent: HELIO_ID
-    child: List[HELIO_ID]
-    size_subtree: int
+
 
 class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
-    cable_dict: Dict[CABLE_ID, CABLE]
-    helio_dict: Dict[HELIO_ID, heliostat]
-    center:heliostat
+    cable_dict: Dict[CABLE_ID, CABLE] = {}
+    helio_dict: Dict[HELIO_ID, heliostat] = {}
     center_id:HELIO_ID
     def __init__(self):
-        self.get_data()
         self.center_id = 0
         p0 = POS(0,0)
         h0 = heliostat(p0)
         self.helio_dict[0] = h0
+        self.get_data()
+        print("structure initialize complete")
         return
 
 
@@ -61,18 +66,18 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
             return False
         return True
 
-    def is_st(self)->bool:
+    def is_st(self)->bool: # check whether it is a spanning tree
         num_nodes = len(self.helio_dict)
         if(self.helio_dict[self.center_id].size_subtree == num_nodes) and (len(self.cable_dict) == num_nodes -1):
             return True
         else:
             return False
 
-    def is_overlap_c2c(self, c1:CABLE_ID, c2:CABLE_ID)->bool:
-        A:POS = self.helio_dict[self.cable_dict[c1].h1].pos
-        B:POS = self.helio_dict[self.cable_dict[c1].h2].pos
-        C:POS = self.helio_dict[self.cable_dict[c2].h1].pos
-        D:POS = self.helio_dict[self.cable_dict[c2].h2].pos
+    def is_overlap_c2c(self, c1:CABLE, c2:CABLE)->bool:
+        A:POS = self.helio_dict[c1.h1].pos
+        B:POS = self.helio_dict[c1.h2].pos
+        C:POS = self.helio_dict[c2.h1].pos
+        D:POS = self.helio_dict[c2.h2].pos
         def vec(p1:POS,p2:POS):
             return np.array([p2.x-p1.x, p2.y-p1.y])
         AC = vec(A,C)
@@ -83,15 +88,24 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
         DB = vec(D,B)
         CA = vec(C,A)
         CB = vec(C,B)
-        if(m.sin(LA.norm(AC*AD))*m.sin(LA.norm(BC*BD))<=0 and m.sin(LA.norm(DA*DB))*m.sin(LA.norm(CA*CB))<=0):
-            return True
-        else:
-            return False
 
-    def is_overlap_c(self, c:CABLE_ID)->List[CABLE_ID]:
+        def equal(p1:POS, p2:POS)->bool:
+            if p1.x == p2.x and p1.y == p2.y:
+                return True
+            else:
+                return False
+        if equal(A,C) or equal(A,D) or equal(B,C) or equal(B,D):
+            return False
+        else:
+            if(m.sin(LA.norm(AC*AD))*m.sin(LA.norm(BC*BD))<=0 and m.sin(LA.norm(DA*DB))*m.sin(LA.norm(CA*CB))<=0):
+                return True
+            else:
+                return False
+
+    def is_overlap_c(self, c:CABLE)->List[CABLE_ID]:
         overlap_cable: List[CABLE_ID] = []
         for cable_id in self.cable_dict:
-            if(self.is_overlap_c2c(cable_id,c)):
+            if(self.is_overlap_c2c(self.cable_dict[cable_id],c)):
                 overlap_cable.append(cable_id)
         return overlap_cable
 
@@ -100,7 +114,7 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
         for cable1 in self.cable_dict:
             for cable2 in self.cable_dict:
                 if cable1 < cable2:
-                    if self.is_overlap_c2c(cable1,cable2):
+                    if self.is_overlap_c2c(self.cable_dict[cable1],self.cable_dict[cable2]):
                         overlap_cable_pair.append([cable1,cable2])
         return overlap_cable_pair
 
@@ -135,7 +149,7 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
                 x = float(data[0])
                 y = float(data[1])
                 p = POS(x,y)
-                new_hs = heliostat(POS = p)
+                new_hs = heliostat(pos= p)
                 self.helio_dict[max(self.helio_dict.keys())+1]= new_hs
         return
 
@@ -143,6 +157,18 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
         pass
 
     def visualise(self):
+        x = [h.pos.x for h in self.helio_dict.values()]
+        y = [h.pos.y for h in self.helio_dict.values()]
+        #ax.plot([0,0],[0,800],c='g') plot cable
+        for c in self.cable_dict.values():
+            pos1 = self.helio_dict[c.h1].pos
+            pos2 = self.helio_dict[c.h2].pos
+            x_list = [pos1.x, pos2.x]
+            y_list = [pos1.y, pos2.y]
+            plt.plot(x_list, y_list,c='g')
+        plt.scatter(x,y,s=20 ,c='b')
+        plt.scatter([0], [0], [50], c='r')
+        plt.show()
         pass
 
     def load_solution(self):
@@ -157,24 +183,28 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
         dist = m.sqrt((a.x-b.x)**2+(a.y-b.y)**2)
         return dist
 
-    def connect_hs(self,a: HELIO_ID, b: HELIO_ID):
-        if self.helio_dict[self.helio_dict[b].parent] != -1:
+    def connect_hs(self, a: HELIO_ID, b: HELIO_ID):
+        #print(self.helio_dict[b].parent)
+        #print("a: {}, b: {}".format(str(a),str(b)))
+        if self.helio_dict[b].parent != -1:
             if(not self.set_root_st(b)):
                 print("b is connected to the center can't set b as root")
                 return False
         self.helio_dict[a].child.append(b)
         self.helio_dict[b].parent = a
         cur_helio:HELIO_ID = a
-        while (cur_helio != self.center_id):
+        while (cur_helio != self.center_id and cur_helio != -1):
+            # print("current helio: {}".format(str(cur_helio)))
             self.helio_dict[cur_helio].size_subtree += self.helio_dict[b].size_subtree
             cur_helio = self.helio_dict[cur_helio].parent
-        new_cable:CABLE
-        new_cable.h1 = a
-        new_cable.h2 = b
-        self.cable_dict[max(self.cable_dict.keys())+1] = new_cable
+        new_cable:CABLE = CABLE(a,b)
+        if len(self.cable_dict):
+            self.cable_dict[max(self.cable_dict.keys())+1] = new_cable
+        else:
+            self.cable_dict[1] = new_cable
         return True
 
-    def disconnect_hh(self, a:HELIO_ID, b:HELIO_ID):
+    def disconnect_hs(self, a:HELIO_ID, b:HELIO_ID):
         parent_id:HELIO_ID
         child_id:HELIO_ID
         if(self.helio_dict[b].parent == a):
@@ -193,9 +223,12 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
             cur_helio = self.helio_dict[cur_helio].parent
         self.helio_dict[child_id].parent = -1
 
+        del_id = 0
         for cable_id in self.cable_dict:
             if(self.cable_dict[cable_id].h1==a and self.cable_dict[cable_id].h2 ==b) or (self.cable_dict[cable_id].h2==a and self.cable_dict[cable_id].h1==b):
-                del self.cable_dict[cable_id]
+                del_id = cable_id
+        if del_id:
+            del self.cable_dict[del_id]
 
         return True
 
@@ -250,6 +283,39 @@ class Helio_struct: # TODO: 将中心点排除在所有点遍历之外
             self.helio_dict[self.helio_dict[h].parent] = -1
             return True
 
+
+helio = Helio_struct()
+# for i in helio.helio_dict:
+#     if i >0:
+#         helio.connect_hs(i-1,i)
+# print(helio.is_feasible())
+# print(helio.is_overlap())
+# print(helio.is_overdeg())
+# print(helio.is_overlength())
+# print(helio.is_st())
+while not helio.is_st():
+    print("new epoch")
+    for h_id,h in zip(helio.helio_dict.keys(),helio.helio_dict.values()):
+        if h_id != helio.center_id and h.parent == -1: # don't handle center
+            min_dist = 10000
+            parent_id = -1
+            for id in helio.helio_dict:
+                dist = helio.distance(h_id, id)
+                if dist<min_dist and (helio.helio_dict[id].parent != -1 or id == helio.center_id):
+                    new_c = CABLE(h_id, id)
+                    print("id:{},h_id:{}".format(str(id),str(h_id)))
+                    if len(helio.is_overlap_c(new_c))==0:
+                        min_dist = dist
+                        parent_id = id
+            if min_dist < 10000:
+                helio.connect_hs(parent_id,h_id)
+                # #print("id:{}, id parent:{}, h_id:{}, h_id parent:{}".format(str(parent_id),str(helio.helio_dict[parent_id].parent),
+                #                                                             str(h_id), str(helio.helio_dict[h_id].parent)))
+            else:
+                print("can't find suitable parent")
+                print("h_id:{}, h_id parent:{}".format(str(h_id),str(helio.helio_dict[h_id].parent)))
+
+helio.visualise()
 
 
 
